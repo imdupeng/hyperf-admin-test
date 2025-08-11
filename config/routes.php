2884ce6b1dfe862852edd9ed359a2a64c1288020
dupeng <412499907@qq.com>
2025-08-11 11:06:42 +0800

    first commit

---
+++ config/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\HttpServer\Router\Router;
+
+Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@index');
+Router::addGroup('/test/',function (){
+    Router::get('index','App\Controller\UserController@index');
+    Router::post('store','App\Controller\UserController@store');
+    Router::get('update','App\Controller\UserController@update');
+    Router::post('delete','App\Controller\UserController@delete');
+});
+
+Router::get('/favicon.ico', function () {
+    return '';
+});
